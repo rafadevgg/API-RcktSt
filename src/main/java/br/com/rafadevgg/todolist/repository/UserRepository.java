027815355b0091190f7d89850ab0e88f6d9964bf
rafadevgg <rafaelpc5671@gmail.com>
2025-11-07 10:28:59 -0300

    Fix: adjusting authentication filter

--- src/main/java/br/com/rafadevgg/todolist/repository/UserRepository.java
+++ src/main/java/br/com/rafadevgg/todolist/repository/UserRepository.java
@@ -3,8 +3,10 @@ package br.com.rafadevgg.todolist.repository;
 import br.com.rafadevgg.todolist.entity.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserModel, Long> {
 
-    UserModel findByUsername(String username);
-
+    Optional<UserModel> findByUsername(String username);
+    
 }
